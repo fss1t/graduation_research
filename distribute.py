8c91831e3b230af32f78edbea770f496bafa731b
hebikubo <104176342+hebikubo@users.noreply.github.com>
2022-06-06 11:08:55 +0900

    Add files via upload

--- distribute.py
+++ distribute.py
@@ -1,7 +1,17 @@
+"""tool for copying folder tree"""
+
 import sys
 import os
 from pathlib import Path
 from shutil import copy, copytree
+
+
+def main():
+    #dir_o = "CNNConformer_ASR"
+    #dir_o = "JDCNet"
+    dir_o = "StarGANv2VC"
+    dir_c = dir_o + "_c"
+    distribute(dir_o, dir_c)
 
 
 def distribute(dir_o, dir_c):
@@ -9,7 +19,7 @@ def distribute(dir_o, dir_c):
 
     path_o = Path(dir_o)
 
-    list_path_file = [*list(path_o.glob("**/*.py")), *list(path_o.glob("**/*.json"))]
+    list_path_file = [*list(path_o.glob("**/*.py")), *list(path_o.glob("**/*.json"))]  # glob except checkpoints, logs
     for path_file_o in list_path_file:
         path_dir_c = Path(str(path_file_o).replace(dir_o, dir_c)).parent
         path_dir_c.mkdir(parents=1, exist_ok=1)
@@ -17,8 +27,4 @@ def distribute(dir_o, dir_c):
 
 
 if __name__ == "__main__":
-    #dir_o = "CNNConformer_ASR"
-    #dir_o = "JDCNet"
-    dir_o = "StarGANv2VC"
-    dir_c = dir_o + "_c"
-    distribute(dir_o, dir_c)
+    main()
